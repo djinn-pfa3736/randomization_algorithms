e1f5cd315431a5c4747566e56475bdb64bdc8513
satoshi <gajyakichi@gmail.com>
2020-01-04 14:55:16 +0900

    datamanagerにmethodでinput_caseを追加した

--- controller/datamanager.py
+++ controller/datamanager.py
@@ -1,7 +1,9 @@
 # Import packages
+import logging
 import os
 import sqlite3
-import logging
+
+from mysql.connector import cursor
 
 # Set directory
 # *  directory is set to as data manager
@@ -18,14 +20,19 @@ class DataManager(object):
         logging.info(msg='Connecting database' + 'from' + os.getcwd())
         conn.close()
 
-    def input_case(self, hospital, patient_name, group)
+    def input_case(self):
+        conn = sqlite3.connect('../data/patient.db')
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO patient VALUES (2,'Tomishiro','Kinjyo','Control')")
+        conn.commit()
+        conn.close()
 
     def __del__(self):
-        conn = sqlite3.connect('../data/' + self.n_db + '.db')
+        conn = sqlite3.connect('../data/patient.db')
         conn.close()
         logging.info(msg='Disconnecting database')
 
 
 data_manager = DataManager()
-data_manager.DB =
-del data_manager
+data_manager.input_case()
