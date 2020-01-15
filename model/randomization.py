2eed7ac2f1cb3007583867e6c54bf5324eb8cdea
satoshi <gajyakichi@gmail.com>
2020-01-15 23:01:38 +0900

    randomizationをパッケージ化した

--- model/randomization.py
+++ model/randomization.py
@@ -19,10 +19,10 @@ class Randomize (object):
     def simple_randomization_ver1(self):
          # * define AssinedGroup as global variable
         get_study_groups = get_groups_from_json.GetStudyGroups()
-        get_study_groups.get_var_Groups_ver1()
-        print('jsonから'+Groups['study_groups'][0]+'のグループ名を得ました')
-        print('jsonから'+Groups['study_groups'][1]+'のグループ名を得ました')
-        
+        Groups=get_study_groups.get_var_Groups_ver1()
+        # print('jsonから'+Groups['study_groups'][0]+'のグループ名を得ました')
+        # print('jsonから'+Groups['study_groups'][1]+'のグループ名を得ました')
+
  #       global AssinedGroup
 
         if random.random() > 0.5:
