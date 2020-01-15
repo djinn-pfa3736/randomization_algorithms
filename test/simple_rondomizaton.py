e7aa1fb2f5b38ed42f008645b660e214fc39fda7
satoshi <gajyakichi@gmail.com>
2020-01-15 12:54:47 +0900

    リストから治療群名をひっぱってくる

--- test/simple_rondomizaton.py
+++ test/simple_rondomizaton.py
@@ -1,9 +1,11 @@
 # Import package
 import random
-
+import json
 # Global変数としてassignされたGroupを代入する
 # Assigned group is set as global variable
-
+Groups = ['Treat','Control']
+for i in Groups:
+    print (i)
 
 class Randomize (object):
     def __init__(self):
@@ -11,12 +13,12 @@ class Randomize (object):
         global AsssinedGroup
     def simple_randomization_ver1(self):
         if random.random() > 0.5:
-            print('Aに割り当てられました')
-            AssinedGroup = 'A'
+            print(Groups[0]+'に割り当てられました')
+            AssinedGroup = Groups[0]
             print (AssinedGroup)
         else:
-            print('Bに割り当てられました')
-            AssinedGroup = 'B'
+            print(Groups[1]+'に割り当てられました')
+            AssinedGroup = Groups[1]
             print (AssinedGroup)
 
 
