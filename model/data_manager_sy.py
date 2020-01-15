4cf94584f9b4ae8bd269d9a402505462bc4a0788
satoshi <gajyakichi@gmail.com>
2020-01-15 22:55:33 +0900

    randomizationを編集

--- model/data_manager_sy.py
+++ model/data_manager_sy.py
@@ -1,10 +1,9 @@
 # Import packages
 import logging
 import os
-import pdb
-import random
 import sqlite3
 import sys
+import randomization
 
 # Logging setting
 formatter = '%(levelname)s : %(asctime)s :%(message)s'
@@ -15,52 +14,51 @@ logging.basicConfig(level=logging.INFO, format=formatter)
 # *  directory is set to as data manager
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# class DataManager(object):
+randomization=randomization.Randomize()
+randomization.simple_randomization_ver1()
+
+# class DataManager:
+#     def __init__(self):
+#         os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+#         self.conn = sqlite3.connect("../data/patient.db")
+#         self.cursor = self.conn.cursor()
+
+#     def connection_close(self):
+#         self.conn.close()
+
+#     def add_case(self):
+
+#         self.cursor.execute(
+#             "SELECT * FROM patient INNER JOIN assignment ON patient.id = assignment.id")
+
+#     def print_db(self):
+#         for row in self.cursor.fetchall():
+#             print(row)
+
+#     def __del__(self):
+#         conn = sqlite3.connect("../data/patient.db")
+#         conn.close()
+#         logging.info(msg='Disconnecting database')
 
 
-class DataManager:
-    def __init__(self):
-        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+# if __name__ == "__main__":
+#     # *  directory is set to as data manager
+#     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-        self.conn = sqlite3.connect("../data/patient.db")
-        self.cursor = self.conn.cursor()
+#     data_manager = DataManager()
+#     # pdb.set_trace()
 
-    def connection_close(self):
-        self.conn.close()
+#     data_manager.cursor.execute(
+#         "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Gushiken', '0')")
+#     data_manager.cursor.execute(
+#         "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Ueda', '0')")
+#     data_manager.cursor.execute(
+#         "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Tamaki', '0')")
+#     data_manager.cursor.execute(
+#         "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Shiroma', '0')")
+#     data_manager.cursor.execute(
+#         "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Afuso', '0')")
 
-    def add_case(self):
-
-            
-        self.cursor.execute(
-            "SELECT * FROM patient INNER JOIN assignment ON patient.id = assignment.id")
-
-    def print_db(self):
-        for row in self.cursor.fetchall():
-            print(row)
-
-    def __del__(self):
-        conn = sqlite3.connect("../data/patient.db")
-        conn.close()
-        logging.info(msg='Disconnecting database')
-
-
-if __name__ == "__main__":
-    # *  directory is set to as data manager
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-    data_manager = DataManager()
-    # pdb.set_trace()
-
-    data_manager.cursor.execute(
-        "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Gushiken', '0')")
-    data_manager.cursor.execute(
-        "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Ueda', '0')")
-    data_manager.cursor.execute(
-        "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Tamaki', '0')")
-    data_manager.cursor.execute(
-        "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Shiroma', '0')")
-    data_manager.cursor.execute(
-        "insert into patient(hospital, patient_name, exclusion) values('Nakagami', 'Afuso', '0')")
-
-    data_manager.simple_randomization()
-    data_manager.print_db()
+#     data_manager.simple_randomization()
+#     data_manager.print_db()
