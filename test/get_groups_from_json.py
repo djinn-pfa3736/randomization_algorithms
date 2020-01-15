f48fc2c81599f6e8e64671e1ec5767df1707f90b
satoshi <gajyakichi@gmail.com>
2020-01-15 19:03:34 +0900

    get_groups_from_json.pyを追加した

--- test/get_groups_from_json.py
+++ test/get_groups_from_json.py
@@ -3,10 +3,19 @@
 import json
 import os
 
-# *  directory is set 
+# *  directory is set
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-#Load json file
-study_group1 = open ('study_data.json','r')
-study_group2 = json.load(study_group1)
-print(study_group2)
+# Load json file
+study_group_file = open('study_data.json', 'r')
+study_group_object = json.load(study_group_file)
+print(study_group_object)
+print(study_group_object["study_groups"][0])
+
+
+class GetStudyGroup(object):
+    def __int__(self):
+        study_group_file = open('study_data.json', 'r')
+        study_group_object = json.load(study_group_file)
+        global Groups
+        Groups = study_group_object["study_groups"]
