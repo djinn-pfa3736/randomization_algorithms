956e9a87767559d4356f72a6dcfe8402c5283f74
satoshi <gajyakichi@gmail.com>
2020-01-15 22:42:20 +0900

    一応できましたsimple_randomization_ver1

--- model/simple_randomizaton.py
+++ model/simple_randomizaton.py
@@ -18,23 +18,27 @@ if __name__ == '__main__':
 
 
 class Randomize (object):
-    def __init__(self):
-        # * define AssinedGroup as global variable
+    # def __init__(self):
+    #     # * define AssinedGroup as global variable
+    #     get_study_groups = get_groups_from_json.GetStudyGroups()
+    #     get_study_groups.get_var_Groups_ver1()
+    #     print('jsonから'+Groups['study_groups'][0]+'のグループ名を得ました')
+    #     print('jsonから'+Groups['study_groups'][1]+'のグループ名を得ました')
+
+    def simple_randomization_ver1(self):
+         # * define AssinedGroup as global variable
         get_study_groups = get_groups_from_json.GetStudyGroups()
         get_study_groups.get_var_Groups_ver1()
         print('jsonから'+Groups['study_groups'][0]+'のグループ名を得ました')
         print('jsonから'+Groups['study_groups'][1]+'のグループ名を得ました')
-
-    def simple_randomization_ver1(self):
-        if random.random() > 0.5:
-            print(Groups[0]+'に割り当てられました')
-            AssignedGroup = Groups[0]
+        global AssinedGroup
+        
+        if random.random() > 0.5:  
+            AssignedGroup = Groups['study_groups'][0]
             print(AssignedGroup)
         else:
-            print(Groups[1]+'に割り当てられました')
-            AssignedGroup = Groups[1]
+            AssignedGroup = Groups['study_groups'][1]
             print(AssignedGroup)
-
 
 if __name__ == '__main__':
     randomization = Randomize()
