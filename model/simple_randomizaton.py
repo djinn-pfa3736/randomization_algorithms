fe3fd0a5ac33eef1b5470cd22005771e7aa9bbc0
satoshi <gajyakichi@gmail.com>
2020-01-15 22:31:11 +0900

    simple_randomizationを更新

--- model/simple_randomizaton.py
+++ model/simple_randomizaton.py
@@ -1,6 +1,6 @@
 # Import package
-import random
 import json
+import random
 
 import get_groups_from_json
 
@@ -16,26 +16,26 @@ if __name__ == '__main__':
     print(Groups['study_groups'][0])
     print(Groups['study_groups'][1])
 
-# class Randomize (object):
-#     def __init__(self):
-#         # * define AssinedGroup as global variable
-#         global AsssignedGroup
-#         get_study_groups = get_groups_from_json.GetStudyGroups()
-#         get_study_groups.get_var_Groups_ver1()
-#         print('jsonから'+Groups['study_groups'][0]+'のグループ名を得ました')
-#         print('jsonから'+Groups['study_groups'][1]+'のグループ名を得ました')
-#         AssingedGroup
-#     def simple_randomization_ver1(self):
-#         if random.random() > 0.5:
-#             print(Groups[0]+'に割り当てられました')
-#             AssignedGroup = Groups[0]
-#             print(AssginedGroup)
-#         else:
-#             print(Groups[1]+'に割り当てられました')
-#             AssignedGroup = Groups[1]
-#             print(AssginedGroup)
+
+class Randomize (object):
+    def __init__(self):
+        # * define AssinedGroup as global variable
+        get_study_groups = get_groups_from_json.GetStudyGroups()
+        get_study_groups.get_var_Groups_ver1()
+        print('jsonから'+Groups['study_groups'][0]+'のグループ名を得ました')
+        print('jsonから'+Groups['study_groups'][1]+'のグループ名を得ました')
+
+    def simple_randomization_ver1(self):
+        if random.random() > 0.5:
+            print(Groups[0]+'に割り当てられました')
+            AssignedGroup = Groups[0]
+            print(AssignedGroup)
+        else:
+            print(Groups[1]+'に割り当てられました')
+            AssignedGroup = Groups[1]
+            print(AssignedGroup)
 
 
-# if __name__ == '__main__':
-#     randomization = Randomize()
-#     randomization.simple_randomization_ver1()
+if __name__ == '__main__':
+    randomization = Randomize()
+    randomization.simple_randomization_ver1()
