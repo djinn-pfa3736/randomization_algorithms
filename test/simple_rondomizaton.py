b46eac97bbf1339e4e22f2fa5885347dab4797cd
satoshi <gajyakichi@gmail.com>
2020-01-15 12:50:08 +0900

    simple _randomizationをテストOK

--- test/simple_rondomizaton.py
+++ test/simple_rondomizaton.py
@@ -4,12 +4,21 @@ import random
 # Global変数としてassignされたGroupを代入する
 # Assigned group is set as global variable
 
+
 class Randomize (object):
+    def __init__(self):
+        #* define AssinedGroup as global variable
+        global AsssinedGroup
     def simple_randomization_ver1(self):
         if random.random() > 0.5:
             print('Aに割り当てられました')
+            AssinedGroup = 'A'
+            print (AssinedGroup)
         else:
             print('Bに割り当てられました')
+            AssinedGroup = 'B'
+            print (AssinedGroup)
 
-simple_randomization=Randomize()
-simple_randomization.simple_randomization_ver1()
+
+randomization = Randomize()
+randomization.simple_randomization_ver1()
