26d2404b8ba833baeab7b7283531815283ac9dde
satoshi <gajyakichi@gmail.com>
2020-01-15 22:45:16 +0900

    少しsimple_randomization_ver1を整理しました

--- model/simple_randomizaton.py
+++ model/simple_randomizaton.py
@@ -4,26 +4,17 @@ import random
 
 import get_groups_from_json
 
-# Global変数としてassignされたGroupを代入する
-# Assigned group is set as global variable
-# Groups = ['Treat', 'Control']
-# for i in Groups:
-#     print(i)
+# Test
 if __name__ == '__main__':
     get_study_groups = get_groups_from_json.GetStudyGroups()
     Groups = get_study_groups.get_var_Groups_ver1()
     print(Groups)
     print(Groups['study_groups'][0])
     print(Groups['study_groups'][1])
+# Define Randomize class
 
 
 class Randomize (object):
-    # def __init__(self):
-    #     # * define AssinedGroup as global variable
-    #     get_study_groups = get_groups_from_json.GetStudyGroups()
-    #     get_study_groups.get_var_Groups_ver1()
-    #     print('jsonから'+Groups['study_groups'][0]+'のグループ名を得ました')
-    #     print('jsonから'+Groups['study_groups'][1]+'のグループ名を得ました')
 
     def simple_randomization_ver1(self):
          # * define AssinedGroup as global variable
@@ -31,14 +22,16 @@ class Randomize (object):
         get_study_groups.get_var_Groups_ver1()
         print('jsonから'+Groups['study_groups'][0]+'のグループ名を得ました')
         print('jsonから'+Groups['study_groups'][1]+'のグループ名を得ました')
-        global AssinedGroup
         
-        if random.random() > 0.5:  
+ #       global AssinedGroup
+
+        if random.random() > 0.5:
             AssignedGroup = Groups['study_groups'][0]
             print(AssignedGroup)
         else:
             AssignedGroup = Groups['study_groups'][1]
             print(AssignedGroup)
+
 
 if __name__ == '__main__':
     randomization = Randomize()
