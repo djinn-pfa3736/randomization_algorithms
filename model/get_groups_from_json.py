5c505c3c4e59f8886343360a4891bcbc28f03809
satoshi <gajyakichi@gmail.com>
2020-01-16 22:57:34 +0900

    global 変数を使わないで設定した get_groups_from_json.py

--- model/get_groups_from_json.py
+++ model/get_groups_from_json.py
@@ -3,29 +3,18 @@
 import json
 import os
 
-# os.chdir(os.path.dirname(os.path.abspath(__file__)))
-# # Load json file
-# study_group_file = open('study_data.json', 'r')
-# study_group_object = json.load(study_group_file)
-# print(study_group_object)
-# print(study_group_object["study_groups"])
-# print(study_group_object["study_groups"][0])
-
 
 class GetStudyGroups(object):
 
     def get_var_Groups_ver1(self):
-        global Groups
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        study_group_file = open('study_data.json', 'r')
-        study_group_object = json.load(study_group_file)
-        print(study_group_object)
-        Groups = study_group_object
-        return Groups
+        self.study_group_file = open('study_data.json', 'r')
+        self.study_group_object = json.load(self.study_group_file)
+        return self.study_group_object
 
 if __name__ == '__main__':
     get_study_groups = GetStudyGroups()
-    get_study_groups.get_var_Groups_ver1()
+    Groups = get_study_groups.get_var_Groups_ver1()
     print(Groups['study_groups'][0])
     print(Groups['study_groups'][1])
     print(type(Groups))
