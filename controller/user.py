4ef1db7c2262ad3f3a291c2b6193d6da36cd1653
satoshi <gajyakichi@gmail.com>
2020-01-17 21:59:36 +0900

    userからインポートしたくてはまった

--- controller/user.py
+++ controller/user.py
@@ -1,19 +1,25 @@
-# Packaage
 
+
+
+# Packaage
 import tkinter as tk
-import 
-data_manag
+import os, sys
 # Set Directory
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+from ..model import data_manager
+
+# dm = DataManager()
+# dm.print_db()
 
 
-# Window文字
-root = tk.Tk()
-w = tk.Label(root, text="単純ランダム化プログラム")
-w.pack()
 
-# ボタン設定
-button = tk.Button(frame,
-                   text="Add case
-                   command= ")
-root.mainloop()
+# # Window文字
+# root = tk.Tk()
+# w = tk.Label(root, text="単純ランダム化プログラム")
+# w.pack()
+
+# # ボタン設定
+# # button = tk.Button(frame,
+# #                    text="Add case"
+# #                    command= dm.print_db())
+# root.mainloop()
